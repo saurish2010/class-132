275d004b4d6adf6765c9a89a3c498c22af9b18c2
saurish2010 <78722486+saurish2010@users.noreply.github.com>
2022-06-01 14:43:21 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+img="";
+status="";
+function preload(){
+    img=loadImage('dog_cat.jpg');
+}
+function setup(){
+    canvas=createCanvas(640,420);
+    canvas.center();
+     objectDetector=ml5.objectDetector('cocossd',modelLoaded);
+     document.getElementById("status").innerHTML="status:detecting objects";
+
+}
+function modelLoaded(){
+    console.log("modelLoaded");
+    status=true;
+    objectDetector.detect(img,gotResult);
+}
+function gotResult(results){
+    if(error){
+        console.log(error);
+    }
+    console.log(results);
+}
+
+function draw(){
+    image(img,0,0,640,420);
+fill("#FF0000");
+text('dog',45,75);
+noFill();
+stroke("#FF0000");
+rect(30,60,450,350);
+  
+fill("#FF0000");
+text('cat',320,120);
+noFill();
+stroke("#FF0000");
+rect(300,90,270,320);
+ 
+
+}
